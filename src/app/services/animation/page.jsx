c0f42228095bcdfe3036a46104a9249a9c3a5031
SergWebAcceptance <serg.web@acceptance.ltd>
2025-03-03 19:36:37 +0200

    started working on animation page

---
+++ src/app/services/animation/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AnimationHero from "./components/AnimationHero/AnimationHero";
+
+const AnimationPage = () => {
+  return (
+    <>
+      <AnimationHero />
+    </>
+  );
+};
+
+export default AnimationPage;
