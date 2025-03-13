3d36b07c1c6662d3de3fda63504ba9ca1ecabf86
SergWebAcceptance <serg.web@acceptance.ltd>
2025-03-13 10:16:49 +0200

    fixed orders

--- src/app/(shop)/checkout/_components/Checkout/orderUtils.js
+++ src/app/(shop)/checkout/_components/Checkout/orderUtils.js
@@ -34,14 +34,23 @@ export const handleCreateOrder = async (
 
     await updateUserProfile(userId, data, user, updateUser);
 
+    const items = await Promise.all(
+      cart.map(async (item) => {
+        const { fileurl, filename } = await getFileUrlForProduct(item.id, user);
+        return {
+          product: item.id,
+          quantity: item.quantity,
+          price: item.attributes.price,
+          file_url: fileurl,
+          file_name: filename,
+        };
+      })
+    );
+
     const orderData = {
       orderNumber: `ORD_${Math.floor(Math.random() * 900000) + 100000}`,
       user: { id: userId },
-      items: cart.map((item) => ({
-        product: item.id,
-        quantity: item.quantity,
-        price: item.attributes.price,
-      })),
+      items,
 
       total: totalAmount,
       status: "pending",
@@ -127,5 +136,47 @@ export const updateUserProfile = async (userId, data, user, updateUser) => {
     return await response.data;
   } catch (error) {
     console.error("Error updating user:", error);
+  }
+};
+
+const getFileUrlForProduct = async (productId, user) => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_CMS_URL}/api/products/${productId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user?.token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch product details for product id: ${productId}`
+      );
+    }
+
+    const productData = await response.json();
+
+    console.log("productData", productData);
+
+    const fileurl =
+      productData?.filesurl &&
+      productData.filesurl.length > 0
+        ? productData.filesurl[0].fileurl
+        : null;
+
+    const filename =
+      productData?.filesurl &&
+      productData.filesurl.length > 0
+        ? productData.filesurl[0].filename
+        : null;
+
+    return { fileurl, filename };
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    throw error;
   }
 };
